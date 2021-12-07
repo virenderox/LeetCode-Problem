5e052ff751972a4c31d26e2807c685a8d686f9e5
virenderox <43112861+virenderox@users.noreply.github.com>
2021-12-07 22:23:35 +0530

    Create add.py

---
+++ Linklist/Add Two Numbers/add.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        if l1 is None:
+            return l2
+    
+        if l2 is None:
+            return l1
+        
+        dummyNode = head = ListNode(-1)
+        
+        carry = 0
+        
+        while l1  and l2 :
+            
+            sumDigit = l1.val + l2.val + carry
+            carry = sumDigit // 10
+            sumDigit = sumDigit % 10
+            newNode = ListNode(sumDigit)
+            dummyNode.next = newNode
+            dummyNode = dummyNode.next
+            l1 = l1.next
+            l2 = l2.next
+            
+        while l1:
+            
+            sumDigit = l1.val  + carry
+            
+            carry = sumDigit // 10
+            sumDigit = sumDigit % 10
+            newNode = ListNode(sumDigit)
+            dummyNode.next = newNode
+            dummyNode = dummyNode.next
+            l1 = l1.next
+            
+        while l2:
+            
+            sumDigit =  l2.val + carry
+            carry = sumDigit // 10
+            sumDigit = sumDigit % 10
+            newNode = ListNode(sumDigit)
+            dummyNode.next = newNode
+            dummyNode = dummyNode.next
+            l2 = l2.next
+            
+        if carry == 1:
+            newNode = ListNode(1)
+            dummyNode.next = newNode
+            
+
+    
+        return head.next
+                
+            
