3d1c90a299d7637c0e899302980ab7e812a69d33
virenderox <43112861+virenderox@users.noreply.github.com>
2022-01-18 12:03:03 +0530

    Update tree.py

--- Tree/DFS/Postorder of Binary tree/tree.py
+++ Tree/DFS/Postorder of Binary tree/tree.py
@@ -1,9 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from queue import LifoQueue
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
-        
-        self.postOder(root,ans)
+        stack = LifoQueue()
+        self.postOrderIterative(root,ans,stack)
+        #self.postOder(root,ans)
         return ans
+    
+    def postOrderIterative(self,root,ans,stack):
+        
+        if root == None:
+            return ans
+        
+        stack.put(root)
+        while not(stack.empty()):
+            current = stack.get()
+           # print(current)
+            
+            ans.append(current.val)
+            if current.left != None:
+                stack.put(current.left)
+                
+            if current.right != None:
+                stack.put(current.right)
+                
+            
+                
+        
+        return ans.reverse()
     
     
     def postOder(self,root,ans):
