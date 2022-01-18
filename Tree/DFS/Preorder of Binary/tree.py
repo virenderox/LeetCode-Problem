30a0eb7149638a6c39388d2b07824b5a01136b69
virenderox <43112861+virenderox@users.noreply.github.com>
2022-01-18 12:05:00 +0530

    Update tree.py

--- Tree/DFS/Preorder of Binary/tree.py
+++ Tree/DFS/Preorder of Binary/tree.py
@@ -1,13 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from queue import LifoQueue
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
         ans = []
+        stack = LifoQueue()
         
-        self.preOder(root,ans)
+        self.preOrderIterative(root,ans,stack)
+        # this is recursive way self.preOderRecursive(root,ans)
         return ans
     
     
-    def preOder(self,root,ans):
+    def preOderRecursive(self,root,ans):
         
         if root == None:
             return
@@ -17,3 +27,25 @@ class Solution:
         self.preOder(root.right,ans)
         
         return
+    
+    def preOrderIterative(self,root,ans,stack):
+        
+        if root == None:
+            return ans
+        
+        stack.put(root)
+        while not(stack.empty()):
+            current = stack.get()
+           # print(current)
+            ans.append(current.val)
+            
+            if current.right != None:
+                stack.put(current.right)
+                
+            if current.left != None:
+                stack.put(current.left)
+                
+        
+        return ans
+        
+        
