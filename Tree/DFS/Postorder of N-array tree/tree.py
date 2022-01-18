f71f48d2d7366d443edfef61ec87e07ae26dcb34
virenderox <43112861+virenderox@users.noreply.github.com>
2022-01-18 12:04:07 +0530

    Update tree.py

--- Tree/DFS/Postorder of N-array tree/tree.py
+++ Tree/DFS/Postorder of N-array tree/tree.py
@@ -1,9 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+from queue import LifoQueue
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         
         ans = []
+        stack = LifoQueue()
+        self.postOrderIterative(root,ans,stack)
+        #self.postOder(root,ans)
+        return reversed(ans)
+    
+    def postOrderIterative(self,root,ans,stack):
         
-        self.postOder(root,ans)
+        if root == None:
+            return
+        
+        stack.put(root)
+        while not(stack.empty()):
+            current = stack.get()
+            ans.append(current.val)
+            
+            for child in current.children:
+                
+                if child != None:
+                    stack.put(child)
+                
+        
         return ans
     
     
@@ -18,3 +45,4 @@ class Solution:
         ans.append(root.val)
         
         return
+        
