a6e22cb4012f7b7efb3ee0d5552a625be11f4301
virenderox <43112861+virenderox@users.noreply.github.com>
2022-01-18 12:05:26 +0530

    Update tree.py

--- Tree/DFS/Preorder of N-array tree/tree.py
+++ Tree/DFS/Preorder of N-array tree/tree.py
@@ -1,14 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+from queue import LifoQueue
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
         
-        
+        stack = LifoQueue()
         ans = []
         
-        self.preOder(root,ans)
+        self.preOrderIterative(root,ans,stack)
+        
+        #self.preOderRecursive(root,ans)
+        return ans
+    
+    def preOrderIterative(self,root,ans,stack):
+        
+        if root == None:
+            return
+        
+        stack.put(root)
+        while not(stack.empty()):
+            current = stack.get()
+            ans.append(current.val)
+            
+            for child in reversed(current.children):
+                
+                if child != None:
+                    stack.put(child)
+                
+        
         return ans
     
     
-    def preOder(self,root,ans):
+    def preOderRecursive(self,root,ans):
         
         if root == None:
                 return
@@ -18,7 +46,8 @@ class Solution:
            
             
            # print(ans)
-            self.preOder(child,ans)
+            self.preOderRecursive(child,ans)
         
         return
+        
         
